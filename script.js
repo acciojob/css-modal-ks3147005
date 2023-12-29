ae1b1491f3ec1c0918951d7d08c98d8b1c7045d5
karan sharma <93695328+ks3147005@users.noreply.github.com>
2023-12-29 21:11:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.getElementById("openModal").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "flex";
+});
+
+document.querySelector(".modal").addEventListener("click", function(event) {
+    if (event.target === document.querySelector(".modal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
+});
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+});
